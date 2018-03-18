0de75d80591ce4e9ed7f18c67bb8ee821faa86e1
Eden Juscelino <eden.juscelino@gmail.com>
2018-03-19 00:14:22 +0100

    example working normal

--- projectDjango/projectDjango/settings.py
+++ projectDjango/projectDjango/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'projectDjango.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR,],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -120,3 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    STATIC_DIR,
+
+]
